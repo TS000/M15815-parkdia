8b46da76b9e5667c4af9463f929cc3cdbe9c6453
TS000 <tylersnell@me.com>
2019-08-05 08:49:32 -0600

    removed email side

--- converter.js
+++ converter.js
@@ -19,6 +19,8 @@ fs.readdir('/home/pretzel/Desktop/apiTests/var/sftp/uploads/', function (err, fi
                     for (let i = 0; i < files.length; i++) {
                         const csvFilePath = `/home/pretzel/Desktop/apiTests/var/sftp/uploads/${files[i]}`;
                         const substring = 'RES';
+                        let nuArrayReg = [];
+                        let nuArrayRes = [];
 
                         csv({
                                 trim: true,
@@ -26,75 +28,90 @@ fs.readdir('/home/pretzel/Desktop/apiTests/var/sftp/uploads/', function (err, fi
                             })
                             .fromFile(csvFilePath)
                             .then((jsonObj) => {
+                                
 
                                 const data = jsonObj;
-                                console.log(data)
-                                if (files[i].includes(substring)) {
                                     console.log(`it's reg: ${files[i]}`)
                                     for (let i = 0; i < jsonObj.length; i++) {
-                                        const status = data[i].field1;
-                                        const agent = data[i].field2;
-                                        const agentName = data[i].field3;
-                                        const arrivalDate = data[i].field4;
-                                        const arrivalTime = data[i].field5;
-                                        const bookingDate = data[i].field6;
-                                        const duration = data[i].field7;
-                                        const exitDate = data[i].field8;
-                                        const exitTime = data[i].field9;
-                                        const bookingRef = data[i].field10;
-                                        const bookingTime = data[i].field11;
-                                        const bookingValue = data[i].field12;
-                                        const carParkCode = data[i].field13;
-                                        const carParkDesc = data[i].field14;
-                                        const email = data[i].field15;
-                                        const foreName = data[i].field16;
-                                        const initial = data[i].field17;
-                                        const title = data[i].field18;
-                                        const productCode = data[i].field19;
-                                        const productDesc = data[i].field20;
-                                        const promoCode = data[i].field21;
-                                        const newsletter = data[i].field22;
-                                        const specialOffers = data[i].field23;
-                                        const upgrDesc = data[i].field24;
-                                        const upgrOrigCar = data[i].field25;
-                                        const upgrOrigPrice = data[i].field26;
-                                        const upgrOrigPromo = data[i].field27;
-                                        const surname = data[i].field28;
-                                        // console.log(email);
-                                        // console.log(surname);
-                                        mail.main(status, agent, agentName, arrivalDate, arrivalTime, bookingDate, duration, exitDate, exitTime, bookingRef, bookingTime, bookingValue, carParkCode, carParkDesc, email, foreName, initial, title, productCode, productDesc, promoCode, newsletter, specialOffers, upgrDesc, upgrOrigCar, upgrOrigPrice, upgrOrigPromo, surname).catch(console.error);
+                                         let j = {};
+                                        j['status'] = data[i].field1;
+                                        j['agent'] = data[i].field2;
+                                        j['agentName'] = data[i].field3;
+                                        j['arrivalDate'] = data[i].field4;
+                                        j['arrivalTime'] = data[i].field5;
+                                        j['bookingDate'] = data[i].field6;
+                                        j['duration'] = data[i].field7;
+                                        j['exitDate'] = data[i].field8;
+                                        j['exitTime'] = data[i].field9;
+                                        j['bookingRef'] = data[i].field10;
+                                        j['bookingTime'] = data[i].field11;
+                                        j['bookingValue'] = data[i].field12;
+                                        j['carParkCode'] = data[i].field13;
+                                        j['carParkDesc'] = data[i].field14;
+                                        j['email'] = data[i].field15;
+                                        j['foreName'] = data[i].field16;
+                                        j['initial'] = data[i].field17;
+                                        j['title'] = data[i].field18;
+                                        j['productCode'] = data[i].field19;
+                                        j['productDesc'] = data[i].field20;
+                                        j['promoCode'] = data[i].field21;
+                                        j['newsletter'] = data[i].field22;
+                                        j['specialOffers'] = data[i].field23;
+                                        j['upgrDesc'] = data[i].field24;
+                                        j['upgrOrigCar'] = data[i].field25;
+                                        j['upgrOrigPrice'] = data[i].field26;
+                                        j['upgrOrigPromo'] = data[i].field27;
+                                        j['surname'] = data[i].field28;
+                                        nuArrayReg.push(j);
                                     }
-                                } else {
-                                    console.log(`it's res ${files[i]}`)
-                                    for (let i = 0; i < jsonObj.length; i++) {
-                                        const transType = data[i].field1;
-                                        const address1 = data[i].field2;
-                                        const address2 = data[i].field3;
-                                        const address3 = data[i].field4;
-                                        const countryState = data[i].field5;
-                                        const email = data[i].field6;
-                                        const firstBookDate = data[i].field7;
-                                        const lastBookDate = data[i].field8;
-                                        const newsletter = data[i].field9;
-                                        const postCode = data[i].field10;
-                                        const surname = data[i].field11;
-                                        const title = data[i].field12;
-                                        const town = data[i].field13;
-                                        const userID = data[i].field14;
-                                        const category = data[i].field15;
-                                        // console.log(email);
-                                        // console.log(surname);
-                                        mail2.main(transType, address1, address2, address3, countryState, email, firstBookDate, lastBookDate, newsletter, postCode, surname, title, town, userID, category).catch(console.error);
-                                    }
-                                }
+                                // } else {
+                                    // console.log(`it's res ${files[i]}`)
+                                    // for (let i = 0; i < jsonObj.length; i++) {
+                                    //     let j = {};
+                                    //     j['transType'] = data[i].field1;
+                                    //     j['address1'] = data[i].field2;
+                                    //     j['address2'] = data[i].field3;
+                                    //     j['address3'] = data[i].field4;
+                                    //     j['countryState'] = data[i].field5;
+                                    //     j['email'] = data[i].field6;
+                                    //     j['firstBookDate'] = data[i].field7;
+                                    //     j['lastBookDate'] = data[i].field8;
+                                    //     j['newsletter'] = data[i].field9;
+                                    //     j['postCode'] = data[i].field10;
+                                    //     j['surname'] = data[i].field11;
+                                    //     j['title'] = data[i].field12;
+                                    //     j['town'] = data[i].field13;
+                                    //     j['userID'] = data[i].field14;
+                                    //     j['category'] = data[i].field15;
+                                    //     nuArrayRes.push(j);
+                                    // }
+                                
+                                
                             }).then(() => {
-                                let oldPath = `/home/pretzel/Desktop/apiTests/var/sftp/uploads/${files[i]}`
-                                let newPath = `/home/pretzel/Desktop/apiTests/var/sftp/backups/${files[i]}`
+                                let jsonFile = JSON.stringify(nuArrayReg)
+                                fs.writeFile("/home/pretzel/Desktop/apiTests/var/sftp/uploads/regData.json", jsonFile, function(err) {
+                                    if(err) {
+                                        return console.log(err);
+                                    }
+                                
+                                    console.log("The file was saved REG!");
+                                }); 
 
-                                fs.rename(oldPath, newPath, function (err) {
-                                    if (err) throw err
-                                    console.log('Successfully moved.')
-                                })
+                                // let jsonFile = JSON.stringify(nuArrayRes)
+                                // fs.writeFile("/home/pretzel/Desktop/apiTests/var/sftp/uploads/resData.json", jsonFile, function(err) {
+                                //     if(err) {
+                                //         return console.log(err);
+                                //     }
+                                
+                                //     console.log("The file was saved RES!");
+                                // }); 
+                                // let oldPath = `/home/pretzel/Desktop/apiTests/var/sftp/uploads/${files[i]}`
+                                // let newPath = `/home/pretzel/Desktop/apiTests/var/sftp/backups/${files[i]}`
+
+                                // fs.rename(oldPath, newPath, function (err) {
+                                //     if (err) throw err
+                                //     console.log('Successfully moved.')
+                                // })
                             });
                     };
                 };
